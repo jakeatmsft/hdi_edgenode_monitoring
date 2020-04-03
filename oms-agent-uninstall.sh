c124dc882dac955c48ee92cda1eee5113e076310
jakedubb <jakedubb@users.noreply.github.com>
2020-04-03 14:42:32 -0700

    Update oms-agent-uninstall.sh

--- oms-agent-uninstall.sh
+++ oms-agent-uninstall.sh
@@ -4,5 +4,3 @@
 
 sed -i "s/^exit 101$/exit 0/" /usr/sbin/policy-rc.d 
 wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh --purge
-sudo su rm -rf /etc/opt/microsoft/omsagent 
-sudo su rm -rf/var/opt/microsoft/omsagent
